a133b60457d8b470b7f7f612b662a786affb56ef
TTcoinDeveloper <adamashton11@gmail.com>
2019-05-22 03:30:08 -0300

    new

---
+++ app/peatio/config/initializers/versioning.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+# This file is auto-generated from the current state of VCS.
+# Instead of editing this file, please use bin/gendocs.
+
+module Peatio
+  class Application
+    GIT_TAG =    '1.9.3.pre.rc.28'
+    GIT_SHA =    '81ee8ee'
+    BUILD_DATE = 'Mon Dec 10 13:41:33 UTC 2018'
+    VERSION =    GIT_TAG
+  end
+end
